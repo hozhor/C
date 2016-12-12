d385963a38c7a987cc37abdedbe33f56ee52450a
hozhor <hozhor@gmail.com>
2016-12-12 14:54:06 +0800

    定义一个含有是个元素的整形数组，从键盘中输入这是个元素的值，然后输出到屏幕

---
+++ lianxi49.cpp
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main()
+{
+	int a[10];
+	for (int i = 0; i < 10; ++i)
+	{
+		scanf("%d",&a[i]);
+
+		/* code */
+	}
+	for (int i = 0; i < 10; ++i)
+	{
+		printf("%d  \n",a[i]);
+		
+	}
+	return 0;
+}
