a97de8110f7be045970bd3091142249ca4ff0230
hozhor <hozhor@gmail.com>
2016-12-13 23:51:39 +0800

    画图，学用line画直线。

---
+++ lianxi53.cpp
@@ -0,0 +1,29 @@
+#include "graphics.h"
+main()
+{int driver,mode,i;
+float x0,y0,y1,x1;
+float j=12,k;
+driver=vga;mode=vgahi;
+initgraph(&driver,&mode,"");
+setbkcolor(green);
+x0=263;y0=263;y1=275;x1=275;
+for(i=0;i<=18;i++)
+{
+setcolor(5);
+line(x0,y0,x0,y1);
+x0=x0-5;
+y0=y0-5;
+x1=x1+5;
+y1=y1+5;
+j=j+10;
+}
+x0=263;y1=275;y0=263;
+for(i=0;i<=20;i++)
+{
+setcolor(5);
+line(x0,y0,x0,y1);
+x0=x0+5;
+y0=y0+5;
+y1=y1-5;
+}
+}
