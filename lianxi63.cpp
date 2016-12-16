459e54e6d5de1bbc8959858cab8c3559d785cac8
hozhor <hozhor@gmail.com>
2016-12-16 23:58:16 +0800

    创建一个链表。

---
+++ lianxi63.cpp
@@ -0,0 +1,29 @@
+/*creat a list*/
+#include "stdlib.h"
+#include "stdio.h"
+struct list
+{ int data;
+struct list *next;
+};
+typedef struct list node;
+typedef node *link;
+void main()
+{ link ptr,head;
+int num,i;
+ptr=(link)malloc(sizeof(node));
+ptr=head;
+printf("please input 5 numbers==>\n");
+for(i=0;i<=4;i++)
+{
+　scanf("%d",&num);
+　ptr->data=num;
+　ptr->next=(link)malloc(sizeof(node));
+　if(i==4) ptr->next=null;
+　else ptr=ptr->next;
+}
+ptr=head;
+while(ptr!=null)
+{ printf("the value is ==>%d\n",ptr->data);
+　ptr=ptr->next;
+}
+}
