77dcf6afb0ab98dfa6d9a5d8856a904ca7cb1f8f
hozhor <hozhor@gmail.com>
2016-12-18 23:00:12 +0800

    放松一下，算一道简单的题目。

---
+++ lianxi65.cpp
@@ -0,0 +1,17 @@
+main()
+{
+int i,n;
+for(i=1;i<5;i++)
+{ n=0;
+　if(i!=1)
+　n=n+1;
+　if(i==3)
+　n=n+1;
+　if(i==4)
+　n=n+1;
+　if(i!=4)
+　n=n+1;
+　if(n==3)
+　　printf("zhu hao shi de shi:%c",64+i);
+　}
+}
