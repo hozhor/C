a7600517092f2db661e58776eca1eeba1d886aa1
hozhor <hozhor@gmail.com>
2016-12-19 23:28:47 +0800

    简单的一维数组排序问题

---
+++ array02.cpp
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main(){
+	int a[10];
+	for (int i=0;i<=10;i++){
+	a[i]=i;	
+	printf("%d ",a[i]);
+	}
+	printf("\n");
+	for(int i=10;i>=0;i--){
+	printf("%d ",a[i]);
+		
+	
+	}
+printf("\n");
+return 0;
+}
