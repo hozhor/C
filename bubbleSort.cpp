e02f81a4fa2c002a1af26f907b3229f18d42949b
hozhor <hozhor@gmail.com>
2016-12-21 23:16:03 +0800

    用起泡法对10个数排序（有小到大）

---
+++ bubbleSort.cpp
@@ -0,0 +1,24 @@
+#include <stdio.h>
+int main(){
+	int a[10];
+	int i,j,t;
+	printf("please input 10 numbers:\n");
+	for(i=0;i<10;i++)	
+	scanf("%d",&a[i]);
+	printf("\n");
+	for(j=0;j<9;j++)
+		for(i=0;i<9-j;i++)
+			if(a[i]>a[i+1])
+			{
+			t=a[i];
+			a[i]=a[i+1];
+			a[i+1]=t;
+			}	
+		printf("the sorted nubers:\n");
+		for(i=0;i<10;i++)	
+		printf("%d",a[i]);
+		printf("\n");		
+
+
+return 0;
+}
