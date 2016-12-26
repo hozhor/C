b427ef29f532dce876072d5ac89c85771baf185a
hozhor <hozhor@gmail.com>
2016-12-26 18:02:23 +0800

    把字符串str1的后链接str2

---
+++ StrCat.cpp
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<string.h>
+int main(){
+char str01[]={'m','y','n','a','m','e','i','s'};
+char str03[]={'a','n','w','e','r'};
+printf("%s\n",strcat(str01,str03));
+
+
+
+
+
+
+return 0;
+}
