9891246d7d3f4c5fc420c41f6819743c4bf5d7db
hozhor <hozhor@gmail.com>
2016-12-26 18:08:37 +0800

    把字符串换成小写字母输出

---
+++ stringlowercase.cpp
@@ -0,0 +1,9 @@
+#include<stdio.h>
+#include<string.h>
+
+int main(){
+    char str[] = "HTTP://see.xidian.edu.cn/cpp/u/shipin/";
+    printf("%s\n", strlwr(str));/*把所以的字符串换成小写*/
+    printf("%s\n", str);
+    return  0;
+}
