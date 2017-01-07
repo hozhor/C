c3bbb8529abb7469f07cf9cc20e3d874720a58a6
hozhor <hozhor@gmail.com>
2017-01-07 20:59:16 +0800

    将字符串a复制到字符串b中(用指针)

---
+++ pointerArrayCopy.cpp
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+char a[]="hello world!",b[20];
+int i;
+for(i=0;*(a+i)!='\0';i++)
+*(b+i)=*(a+i);
+*(b+i)='\0';
+printf("string a is :%s\n",a);
+printf("string b is :");
+for (i=0;b[i]!='\0';i++)
+printf("%c",b[i]);
+printf("\n");
+return 0;
+}
