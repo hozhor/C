f8052cb726b1209bdb3dc911d40ba471b5e652fc
hozhor <hozhor@gmail.com>
2017-01-12 23:54:36 +0800

    判断两个字长度,输出长度之差

---
+++ exam2.cpp
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<string.h>
+int main(){
+char string1[20];
+char string2[20];
+scanf("%s",string1);
+scanf("%s",string2);
+
+
+printf("%d\n",strlen(string2)-strlen(string1));
+
+
+
+
+
+return 0;
+}
